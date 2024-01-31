ff0a42b5c1c41c936ee4da76469ab47fd52291c1
IgorLucasBrazz <igorlucassbraz147@gmail.com>
2024-01-31 14:44:44 -0300

    add atualizacoes

---
+++ studies/src/components/Lista/index.tsx
@@ -0,0 +1,24 @@
+import style from './Lista.module.scss'
+import Item from "./item";
+import { ITarefa } from '../../types/tarefa';
+
+
+
+
+function Lista({tarefas}: {tarefas: ITarefa[]}){
+
+    return(
+        <aside className={style.listaTarefas}>
+            <h2>Estudos do dia</h2>
+            <ul>
+                {tarefas.map((item, index)=> (
+                    <Item
+                        key={index} 
+                        {...item} />
+                ))}
+            </ul>            
+        </aside>
+    )
+}
+
+export default Lista;
